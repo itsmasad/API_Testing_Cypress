ef7a3f3b201178a4c0ebae6b33470bb5ee5bac67
itsmasad <muhammadasadofficial@gmail.com>
2024-02-28 17:46:15 +0500

    updated post call request with comparing the body data

---
+++ cypress/e2e/APITesting/PostCalls.cy.js
@@ -0,0 +1,23 @@
+describe("api testing",()=>{
+
+    it("Approach1- Hard coded json object", ()=>{
+
+        const requestBody={
+            name: "morpheus",
+            job: "leader"
+        }
+        cy.request({
+            method: 'POST',
+            url: 'https://reqres.in/api/users',
+            // We have already created the body so we will simply pass it
+            body: requestBody
+        })
+        // to Capture the response we will use .then
+        .then((resoponse)=>{
+            expect(resoponse.status).to.eq(201)
+            expect(resoponse.body.name).to.eq("morpheus")
+            expect(resoponse.body.job).to.eq("leader")
+        }) 
+    })
+
+})
