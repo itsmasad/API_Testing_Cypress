7bd77afb43b63e7ea62e07620ef3d4311b98ef25
itsmasad <muhammadasadofficial@gmail.com>
2024-02-28 18:04:11 +0500

    updated code

--- cypress/e2e/APITesting/PostCalls.cy.js
+++ cypress/e2e/APITesting/PostCalls.cy.js
@@ -20,4 +20,27 @@ describe("api testing",()=>{
         }) 
     })
 
+
+    it("Approach1- Dynamically generating json object", ()=>{
+
+        const requestBody={
+            Rname: Math.random().toString(5).substring(2),
+            // Creating random gmail
+            job: Math.random().toString(5).substring(2)+"@gmail.com",
+        }
+        cy.request({
+            method: 'POST',
+            url: 'https://reqres.in/api/users',
+            // We have already created the body so we will simply pass it
+            body: requestBody
+        })
+        // to Capture the response we will use .then
+        .then((resoponse)=>{
+            expect(resoponse.status).to.eq(201)
+            expect(resoponse.body.name).to.eq(requestBody.Rname)
+            expect(resoponse.body.job).to.eq(requestBody.job)
+        }) 
+    })
+    
+
 })
