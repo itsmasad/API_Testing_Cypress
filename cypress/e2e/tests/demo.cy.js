f36690b3bebf7e12e86d6b34509e35ea3456a05c
itsmasad <muhammadasadofficial@gmail.com>
2024-03-18 16:43:29 +0500

    Updated

---
+++ cypress/e2e/tests/demo.cy.js
@@ -0,0 +1,14 @@
+///<reference types="cypress"/>
+
+describe('this is suite',()=>{
+    it('verify user is able to open google.com',()=>{
+        cy.visit('https://www.google.com');
+    });
+
+    it('verify the element is present or not',()=>{
+        cy.visit('https://www.google.com')
+        cy.get('#APjFqb', { timeout: 10000 }).should('exist');
+        cy.get('#APjFqb').type('Cypress')
+        cy.get('[value="Google Search"]').eq(0).click({force:true})
+    });
+})
