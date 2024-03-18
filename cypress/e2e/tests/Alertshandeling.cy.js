fa691d511c5ec986ba080f1a2dd670c9ef7069ce
itsmasad <muhammadasadofficial@gmail.com>
2024-03-18 18:36:49 +0500

    updated

--- cypress/e2e/tests/Alertshandeling.cy.js
+++ cypress/e2e/tests/Alertshandeling.cy.js
@@ -4,7 +4,13 @@ describe('This is suit for alerts',()=>{
 
     it('First alert test',()=>{
         cy.visit('https://the-internet.herokuapp.com/javascript_alerts')
-        
+        // cy.get(':nth-child(1) > button',{timeout:10000}).should('exist').should('have.text','Click for JS Alert');
+        cy.get(':nth-child(1) > button').click({force:true});
+
+        cy.on('window:alert', (str)=>{
+            expect(str).equal('I am a JS Alert');
+        })
+        cy.get('#result').should('have.text','You successfully clicked an alert')
     })
 
 })
