9c6c889b6d8ff15de641675b03ecb5b84652f7a3
itsmasad <muhammadasadofficial@gmail.com>
2024-03-18 17:12:04 +0500

    updated

--- cypress/e2e/tests/SearchSuggesion.cy.js
+++ cypress/e2e/tests/SearchSuggesion.cy.js
@@ -7,7 +7,7 @@ describe('verify the auto-search suggestion functionality',()=>{
         cy.visit('https://www.amazon.in');
         cy.get('#twotabsearchtextbox').should('exist');
 
-
+        // Creating a function to get all the values in the suggesion
         cy.get('#twotabsearchtextbox').type('iphone').then(function(el){
         cy.get('.left-pane-results-container').children().each(function(el){
             cy.wrap(el).invoke('text').then(function(txt){
---
+++ cypress/e2e/tests/dropdown.cy.js
@@ -0,0 +1,10 @@
+///<reference types="cypress"/>
+
+describe('This is a suit',()=>{
+
+    it('verify the functionality of drop down',()=>{
+        cy.visit('https://the-internet.herokuapp.com/dropdown')
+        
+    })
+
+})
