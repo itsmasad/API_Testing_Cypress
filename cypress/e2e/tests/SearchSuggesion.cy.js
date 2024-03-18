2779315f7101e8e69c247d86e988e121c27abb66
itsmasad <muhammadasadofficial@gmail.com>
2024-03-18 17:07:04 +0500

    updated

--- cypress/e2e/tests/SearchSuggesion.cy.js
+++ cypress/e2e/tests/SearchSuggesion.cy.js
@@ -5,7 +5,20 @@ describe('verify the auto-search suggestion functionality',()=>{
 
     it('verify auto search',()=>{
         cy.visit('https://www.amazon.in');
-        
+        cy.get('#twotabsearchtextbox').should('exist');
+
+
+        cy.get('#twotabsearchtextbox').type('iphone').then(function(el){
+        cy.get('.left-pane-results-container').children().each(function(el){
+            cy.wrap(el).invoke('text').then(function(txt){
+                cy.log(txt)
+                if(txt.trim()=="iphone 12"){
+                    cy.wrap(el).click({force:true});
+                }
+            })
+        })
+      })
+
     })
 
 })
