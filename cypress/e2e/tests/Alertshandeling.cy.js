d262e535078dc227966296959977bc9bbd55f7ab
itsmasad <muhammadasadofficial@gmail.com>
2024-03-18 18:25:26 +0500

    Adding Alerts

---
+++ cypress/e2e/tests/Alertshandeling.cy.js
@@ -0,0 +1,10 @@
+///<reference types="cypress"/>
+
+describe('This is suit for alerts',()=>{
+
+    it('First alert test',()=>{
+        cy.visit('https://the-internet.herokuapp.com/javascript_alerts')
+        
+    })
+
+})
