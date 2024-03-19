f8daf8e663ac167478fe58aaa003c51f44cf85c2
itsmasad <muhammadasadofficial@gmail.com>
2024-03-19 06:50:59 +0500

    updated

--- plugins/index.js
+++ plugins/index.js
@@ -1,9 +1,6 @@
 module.exports = (on, config) => {
-    on('before:browser:launch', (browser, launchOptions) => {
-        console.log('Cypress is launching the browser...');
-    });
-};
-
-setupNodeEvents(on, config) {
     require('cypress-mochawesome-reporter/plugin')(on);
-  }
+    // on('before:browser:launch', (browser, launchOptions) => {
+    //     console.log('Cypress is launching the browser...');
+    // });
+};
