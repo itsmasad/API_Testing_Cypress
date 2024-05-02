ab16850f390154ba4f182ef24c0c07136744f998
itsmasad <muhammadasadofficial@gmail.com>
2024-05-02 10:16:40 +0500

    updated code

--- cypress/e2e/tests/dropdown.cy.js
+++ cypress/e2e/tests/dropdown.cy.js
@@ -6,7 +6,6 @@ describe('This is a suit',()=>{
         cy.visit('https://the-internet.herokuapp.com/dropdown')
         cy.get('#dropdown').should('exist');
         cy.get('h3').should('exist').should('have.text','Dropdown List')
-        
         cy.get('#dropdown').select('Option 2'); 
     })
 
